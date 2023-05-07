39a204190a59ed2dd3505e4cd474ebe167335fc2
sumeettt <sumeetp987@gmail.com>
2023-05-07 15:15:25 +0000

    API 1 done

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const bcrypt = require("bcrypt");
 
 const app = express();
 
+app.use(express.json());
+
 const dbPath = path.join(__dirname, "userData.db");
 
 let db = null;
@@ -27,3 +29,39 @@ const initializeDBAndServer = async () => {
 };
 
 initializeDBAndServer();
+
+//API 1 Register with 3 scenarios
+
+app.post("/register", async (request, response) => {
+  const requestBody = request.body;
+  const { username, name, password, gender, location } = requestBody;
+  console.log(requestBody);
+  const usernameQuery = `
+    SELECT * FROM user WHERE username = '${username}';
+  `;
+  const dbUser = await db.get(usernameQuery);
+  if (dbUser !== undefined) {
+    response.status(400);
+    response.send("User already exists");
+  } else {
+    if (password.length < 5) {
+      response.status(400);
+      response.send("Password is too short");
+    } else {
+      const encryptedPassword = bcrypt.hash(password, 5);
+      const postQuery = `
+        INSERT INTO user(username,name,password,gender, location)
+        VALUES(
+            '${username}',
+            '${name}',
+            '${encryptedPassword}',
+            '${gender}',
+            '${location}'
+        );
+      `;
+      const dbResponse = await db.run(postQuery);
+      response.status(200);
+      response.send("User created successfully");
+    }
+  }
+});

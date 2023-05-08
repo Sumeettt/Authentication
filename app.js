1d3a2a2f5abb123ea3ad2463c26b8d925acca16f
sumeettt <sumeetp987@gmail.com>
2023-05-08 06:00:06 +0000

    all APIs done

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.post("/login", async (request, response) => {
   const dbUser = await db.get(selectUserQuery);
   if (dbUser === undefined) {
     response.status(400);
-    response.send("Invalid User");
+    response.send("Invalid user");
   } else {
     const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
     if (isPasswordMatched === true) {
@@ -87,3 +87,37 @@ app.post("/login", async (request, response) => {
     }
   }
 });
+
+//API 3 change password
+app.put("/change-password", async (request, response) => {
+  const requestBody = request.body;
+  console.log(requestBody);
+  const { username, oldPassword, newPassword } = requestBody;
+  const userSearchQuery = `
+        SELECT * FROM user WHERE username = '${username}';
+    `;
+  const dbUser = await db.get(userSearchQuery);
+  console.log(dbUser);
+  const isPasswordMatched = await bcrypt.compare(oldPassword, dbUser.password);
+  console.log(isPasswordMatched);
+  if (isPasswordMatched === false) {
+    response.status(400);
+    response.send("Invalid current password");
+  } else if (newPassword.length < 5) {
+    response.status(400);
+    response.send("Password is too short");
+  } else {
+    const updatedPassword = await bcrypt.hash(newPassword, 5);
+    console.log(updatedPassword);
+    const updatePasswordQuery = `
+            UPDATE user
+            SET password = '${updatedPassword}'
+            WHERE username = '${username}';
+        `;
+    await db.run(updatePasswordQuery);
+    response.status(200);
+    response.send("Password updated");
+  }
+});
+
+module.exports = app;
